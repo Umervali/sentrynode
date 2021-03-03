4a836ea7d712b0c9ca157e0b153df545ccf8ed28
root <root@Evtmgmt.yd1zpq31oafujmpxx0b20vpa4g.ax.internal.cloudapp.net>
2021-03-03 13:28:58 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://03c7d32c2142492ca360d21d175431e0@o541125.ingest.sentry.io/5659889",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
